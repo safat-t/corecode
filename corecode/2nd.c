f3953f2fc35537ca177f22dc43c95c8c40c1fcc6
safat-t <safattalukder15@gmail.com>
2026-02-17 02:02:08 +0600

    new c

---
+++ corecode/2nd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    float height = 5.8;
+    char grade = 'A';
+
+    printf("Hello, World!\n");
+    printf("My age is %d years.\n", age);
+    printf("My height is %.1f feet.\n", height);
+    printf("My grade is %c.\n", grade);
+
+    return 0;
+}
